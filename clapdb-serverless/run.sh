cb8f1d7612133102922e038d13ad1bbc9c65a20e
longqm <long2008920@163.com>
2024-11-21 10:39:01 +0800

    clapdb convert x-process-time from 'ms' to 's' using awk

--- clapdb-serverless/run.sh
+++ clapdb-serverless/run.sh
@@ -7,7 +7,7 @@ echo "query_num,try,execution_time" >result.csv
 cat queries.sql | while read query; do
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        RES=$(clapctl -n ${deployment} sql -v -s "$query /*dispatch.segment_number=6; groupby_partition_count=36*/" | grep 'x-process-time' | awk '{print $3}')
+        RES=$(clapctl -n ${deployment} sql -v -s "$query /*dispatch.segment_number=6; groupby_partition_count=36*/" | grep 'x-process-time' | awk '{print $3}' | awk '{sub(/ms$/, ""); printf "%.3f", $0 / 1000}')
         if [[ -n "$RES" ]]; then
             echo -n "${RES}"
             echo "${QUERY_NUM},${i},${RES}" >>result.csv
