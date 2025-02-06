df0a4750273fe0520b7bd444bf93d5afe2adbbdc
longqm <long2008920@163.com>
2025-02-06 06:11:30 +0000

    --kernel-page-cache 1

--- clapdb-gcp/run.sh
+++ clapdb-gcp/run.sh
@@ -5,7 +5,7 @@ TRIES=3
 QUERY_NUM=1
 echo "query_num,try,execution_time" >result.csv
 cat queries.sql | while read -r query; do
-    ./clap_node --config ./stdb.toml &>/tmp/null &
+    ./clap_node --kernel-page-cache 1 --config ./stdb.toml &>/tmp/null &
     sleep 1
 
     # echo "${QUERY_NUM} ${query}"
